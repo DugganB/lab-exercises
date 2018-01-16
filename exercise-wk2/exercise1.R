8da5edca3b5570e61f94cc27c3df9ab15f43c7e6
Duggan Burke <dugganb@D-10-18-153-71.dhcp4.washington.edu>
2018-01-16 14:18:25 -0800

    Adds my solutions to the problems from lab.

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -1,7 +1,7 @@
 ## Part 1: Debugging
 
-my.num <- 6
-initials <- "?. ?."
+my.num <- 15
+initials <- "D. B."
 
 my.vector <- c(my.num, initials)
 
@@ -9,7 +9,7 @@ my.vector <- c(my.num, initials)
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# You cannot add chracters to numbers.
 
 install.packages("stringr")
 
@@ -18,36 +18,62 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# We havent put in the library yet, we need to actually add it/initialize it
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# We dont have a variable called 'initial'
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
 
+whatIsThis <- 1
+print(typeof(whatIsThis))
+
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
+CompareLength <- function(vectorOne, vectorTwo) {
+  difference <- abs(nchar(vectorOne) - nchar(vectorTwo))
+  toReturn <- paste("The difference in length is", difference)
+  return(paste("The difference in length is", difference))
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
+print(CompareLength("Hi", "Bye"))
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
 
+DescribeDifference <- function(vectorOne, vectorTwo) {
+  difference <- nchar(vectorOne) - nchar(vectorTwo)
+  if (difference < 0){
+    return(paste("Your second vector is longer by", abs(difference), "elements"))
+  } else {
+    return(paste("Your first vector is longer by", difference, "elements"))
+  }
+}
+
 # Pass two vectors to your `DescribeDifference` function
 
+print(DescribeDifference("Hi", "Bye"))
 
 # Write a function `CombineVectors` that takes in 3 vectors and combines them into one
 
+CombineVector <- function(vecOne, vecTwo, vecThree) {
+  return(c(vecOne, vecTwo, vecThree))
+}
+
 # Send 3 vectors to your function to test it.
+print(CombineVector("Hi", "bye", "lie"))
 
 # Write a function `CapsTime` that takes in a vector of names of courses you're taking, ex "Informatics 201" and removes all capital letters
+CapsTime <- function(courseNames) {
+  return(tolower(courseNames))
+}
 
-
+print(CapsTime("HAH, It doES WoRK"))
